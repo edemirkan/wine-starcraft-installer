b6cbf2c1832f0ff855a09469f61995ae9ef9d810
Evren Demirkan <evren.demirk4n@gmail.com>
2017-05-13 17:48:12 -0400

    Update install script and Readme

--- README.md
+++ README.md
@@ -5,19 +5,29 @@ Wine StarCraft Installer for Ubuntu
 
 ```
 $ ls
-StarCraft-Setup.exe
-wine-sc-installer.sh
-wine-sc-shortcuts.sh
+	
+	StarCraft-Setup.exe
+	wine-sc-installer.sh
+	wine-sc-shortcuts.sh
 
 ```
 2. run "./wine-sc-installer"
 
-`$ ./wine-sc-installer.sh`
+`	./wine-sc-installer.sh`
 
-3. If asked, install "Mono" and "Gecko" packages for the wine prefix. It's recommended to install StarCraft to a folder other than "Program Files", eg. "C:\games". 
+3. If asked, install "Mono" and "Gecko" packages for the wine prefix. It's recommended to install StarCraft to a folder other than "Program Files", eg. "C:\games" and don't launch the game after setup completes installation. 
 
-4. Once the installation is completed, run `wine-sc-shortcuts.sh` to create desktop shortcuts.
+4. Once the installation is completed, run `wine-sc-shortcuts.sh` to create shortcuts.
 
-`$ ./wine-sc-shortcuts.sh`
+`	./wine-sc-shortcuts.sh`
 
-5. Follow the instructions on the terminal.
+5.  Now, you should have 2 new icons in your "Game" category.
+
+	StarCraft
+	StarCraft WineCfg
+
+Using **StarCraft WineCfg**,  add **StarCraft.exe** and enable **Hide Wine version from applications** under staging tab. 
+   
+> Add Application -> Select Starcraft.exe -> Hide Wine version from applications -> OK
+    
+6. Run StarCraft.
--- wine-sc-shortcuts.sh
+++ wine-sc-shortcuts.sh
@@ -9,8 +9,8 @@ SC_WINE_PREFIX=${SC_WINE_PATH}/sc-prefix
 SC_INSTALL_DIR=$(dirname "$(find "${SC_WINE_PREFIX}"/drive_c | grep StarCraft.exe)")
 
 echo ":: Creating StarCraft.desktop"
-#--- create StarCraft launcher on desktop ---
-cat << EOF > "$HOME/Desktop/StarCraft.desktop"
+#--- create StarCraft launcher in .local/share/applications ---
+cat << EOF > "$HOME/.local/share/applications/StarCraft.desktop"
 [Desktop Entry]
 Name=StarCraft
 Exec=env WINEPREFIX="${SC_WINE_PREFIX}" ${SC_WINE_BIN_PATH}/wine StarCraft.exe
@@ -19,11 +19,12 @@ StartupNotify=true
 Comment=Play StarCraft
 Path=${SC_INSTALL_DIR}
 Icon=${SC_WINE_PATH}/starcraft-icon.png
+Categories=Game;
 EOF
 
 echo ":: Creating WineConfig.desktop"
-#--- create StarCraft launcher on desktop ---
-cat << EOF > "$HOME/Desktop/SCWineConfig.desktop"
+#--- create StarCraft launcher in .local/share/applications ---
+cat << EOF > "$HOME/.local/share/applications/SCWineConfig.desktop"
 [Desktop Entry]
 Name=StarCraft WineCfg
 Exec=env WINEPREFIX="${SC_WINE_PREFIX}" ${SC_WINE_BIN_PATH}/winecfg
@@ -31,34 +32,8 @@ Type=Application
 StartupNotify=true
 Comment=Wine StarCraft Configuration
 Icon=${SC_WINE_PATH}/winecfg-icon.png
+Categories=Game;
 EOF
 
-chmod 755 "${HOME}/Desktop/StarCraft.desktop"
-chmod 755 "${HOME}/Desktop/SCWineConfig.desktop"
-
-
-cat << EndOfMessage
-##############################################
-##############################################
-
-
-
-    Now, you should have 2 new icons on your desktop.
-    
-    > StarCraft
-    > StarCraft WineCfg
-
-    Using 'StarCraft WineCfg' set your 'StarCraft.exe' to 'Windows XP' and enable 'Hide Wine version from applications' under staging tab. 
-    
-    Add Application -> Select Starcraft.exe -> win xp -> OK
-    
-    Leave 'Default Settings' as 'Windows 7' otherwise, you'll get crash messages and Battle.net will fail to load.
-
-    If you need help, please check the screenshots here: http://imgur.com/a/qz1GN
-
-
-
-
-##############################################
-##############################################
-EndOfMessage
+chmod 755 "${HOME}/.local/share/applications/StarCraft.desktop"
+chmod 755 "${HOME}/.local/share/applications/SCWineConfig.desktop"
